5546b57cf95a44046e4d602a0cc3e6ffe50bdb04
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-12-19 10:55:28 -0300

    feat: creating the application

---
+++ src/main/java/br/com/combathub/combat_hub/CombatHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.combathub.combat_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombatHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CombatHubApplication.class, args);
+	}
+
+}
