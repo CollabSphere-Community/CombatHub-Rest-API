19501608ebf9c6716b7bc0673d8d4c1856eec2f9
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-12-19 11:04:29 -0300

    feat: user controller

---
+++ src/main/java/br/com/combathub/combat_hub/controller/UserController.java
@@ -0,0 +1,50 @@
+package br.com.combathub.combat_hub.controller;
+
+import br.com.combathub.combat_hub.domain.email_sender.EmailService;
+import br.com.combathub.combat_hub.domain.user.AuthenticationDTO;
+import br.com.combathub.combat_hub.domain.user.UserDTO;
+import br.com.combathub.combat_hub.domain.user.UserEntity;
+import br.com.combathub.combat_hub.domain.user.UserService;
+import br.com.combathub.combat_hub.domain.verification_code.VerificationCodeService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private VerificationCodeService verificationCodeService;
+
+    @PostMapping("/login")
+    public void login(@Valid @RequestBody AuthenticationDTO dto) {
+        var token = new UsernamePasswordAuthenticationToken(dto.login(), dto.password());
+        this.authenticationManager.authenticate(token);
+    }
+
+    @PostMapping("/register")
+    @Transactional
+    public void register(@Valid @RequestBody UserDTO dto) {
+        var encodedPassword = encoder.encode(dto.password());
+        var user = this.userService.registerUser(
+                new UserEntity(dto.login(), encodedPassword, dto.role())
+        );
+        this.verificationCodeService.registerCode(user);
+    }
+
+}
