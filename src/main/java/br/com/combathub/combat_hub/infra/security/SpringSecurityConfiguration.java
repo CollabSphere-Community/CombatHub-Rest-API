50d35bd3fce0329f1d4e767eab87047f65551286
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-12-26 11:38:24 -0300

    feat: defining the endpoints access control rules and the filter's order

--- src/main/java/br/com/combathub/combat_hub/infra/security/SpringSecurityConfiguration.java
+++ src/main/java/br/com/combathub/combat_hub/infra/security/SpringSecurityConfiguration.java
@@ -1,7 +1,9 @@
 package br.com.combathub.combat_hub.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,16 +14,27 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SpringSecurityConfiguration {
 
+    @Autowired
+    private SecurityFilter filter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(rq -> {
+                    rq.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                    rq.requestMatchers(HttpMethod.POST, "/register").permitAll();
+                    rq.requestMatchers(HttpMethod.POST, "verification-code/new-code").permitAll();
+                    rq.anyRequest().authenticated();
+                })
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
