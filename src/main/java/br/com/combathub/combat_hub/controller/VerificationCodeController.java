549eab30356869d918c626cf8f974c8ef270d162
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2025-01-01 11:30:07 -0300

    feat(VerificationCodeController): defining the method for map post request to the /verification-code endpoint

--- src/main/java/br/com/combathub/combat_hub/controller/VerificationCodeController.java
+++ src/main/java/br/com/combathub/combat_hub/controller/VerificationCodeController.java
@@ -4,15 +4,13 @@ import br.com.combathub.combat_hub.domain.user.EmailAddressDTO;
 import br.com.combathub.combat_hub.domain.user.UserEntity;
 import br.com.combathub.combat_hub.domain.user.UserService;
 import br.com.combathub.combat_hub.domain.verification_code.VerificationCodeService;
+import br.com.combathub.combat_hub.infra.exception.UserAlredyConfirmedException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/verification-code")
@@ -32,6 +30,16 @@ public class VerificationCodeController {
         codeService.setExistingCodesAsInvalid(user);
         codeService.registerCode(user);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    @Transactional
+    @PreAuthorize("@verificationCodeService.isCodeValid(#code)")
+    public ResponseEntity verifyCode(@RequestParam String code) throws UserAlredyConfirmedException {
+        var codeEntity = codeService.getCode(code);
+        codeService.setExistingCodesAsInvalid(codeEntity.getUser());
+        userService.confirmUser(codeEntity.getUser());
+        return ResponseEntity.ok().build();
     }
 
 }
