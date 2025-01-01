faa5b025e72cd1efb70fc15c30b9507092c9fe75
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2025-01-01 11:13:38 -0300

    feat(VerificationCodeService): defining methods to get a CodeEntity by the code and to validate a code

--- src/main/java/br/com/combathub/combat_hub/domain/verification_code/VerificationCodeService.java
+++ src/main/java/br/com/combathub/combat_hub/domain/verification_code/VerificationCodeService.java
@@ -2,8 +2,14 @@ package br.com.combathub.combat_hub.domain.verification_code;
 
 import br.com.combathub.combat_hub.domain.email_sender.EmailService;
 import br.com.combathub.combat_hub.domain.user.UserEntity;
+import br.com.combathub.combat_hub.infra.security.VerificationCodeExpiredException;
+import br.com.combathub.combat_hub.infra.security.VerificationCodeInvalidException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Service
 public class VerificationCodeService {
@@ -22,6 +28,31 @@ public class VerificationCodeService {
     public void setExistingCodesAsInvalid(UserEntity user) {
         var codes = repository.findAllByUserAndValidTrue(user);
         codes.forEach(code -> code.setValid(false));
+    }
+
+    public VerificationCodeEntity getCode(String code) {
+        var codeEntity = repository.findByCode(code);
+
+        if(codeEntity == null) {
+            throw new EntityNotFoundException();
+        }
+
+        return codeEntity;
+    }
+
+    public boolean isCodeValid(String code) throws VerificationCodeInvalidException, VerificationCodeExpiredException {
+        var codeEntity = getCode(code);
+        var isCodeExpired = codeEntity
+                .getExpiresAt()
+                .isBefore(LocalDateTime.now(ZoneId.of("-03:00")));
+
+        if(!codeEntity.isValid()) {
+            throw new VerificationCodeInvalidException();
+        } else if(isCodeExpired) {
+            throw new VerificationCodeExpiredException();
+        }
+
+        return true;
     }
 
 }
